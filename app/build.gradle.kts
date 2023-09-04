2fb1d51d3983ad67174ecf024bc491bde2131dbe
이민재 <>
2023-09-05 01:20:23 +0900

    [FEAT] BottomNavigationBar 구현

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,8 @@ dependencies {
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.ext.junit)
     androidTestImplementation(libs.androidx.test.espresso.core)
--- presentation/src/main/java/com/jae464/presentation/MainActivity.kt
+++ presentation/src/main/java/com/jae464/presentation/MainActivity.kt
@@ -3,33 +3,115 @@ package com.jae464.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.materialIcon
+import androidx.compose.material.icons.rounded.Home
+import androidx.compose.material.icons.rounded.Info
+import androidx.compose.material.icons.rounded.List
+import androidx.compose.material.icons.rounded.Settings
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
-import com.jae464.presentation.ui.home.HomeScreen
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.jae464.presentation.home.HomeScreen
+import com.jae464.presentation.setting.SettingScreen
+import com.jae464.presentation.statistic.StatisticScreen
+import com.jae464.presentation.tasks.TaskListScreen
 import com.jae464.presentation.ui.theme.DailyTaskTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             DailyTaskTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    color = MaterialTheme.colorScheme.background
-                ) {
-//                    Greeting("Android")
-                    HomeScreen()
+                val navController = rememberNavController()
+
+                Scaffold(
+                    bottomBar = {BottomNavBar(navController = navController)}
+                ) { padding ->
+                    Row(modifier = Modifier
+                        .padding(padding)
+                        .fillMaxSize()) {
+                        NavigationGraph(navController = navController)
+                    }
                 }
             }
         }
     }
 }
 
+@Composable
+fun BottomNavBar(navController: NavHostController) {
+    val items = Routes.values().asList()
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
+
+    NavigationBar {
+        items.forEach {item ->
+            NavigationBarItem(
+                selected = currentRoute == item.route,
+                onClick = {
+                          navController.navigate(item.route) {
+                              navController.graph.startDestinationRoute?.let {
+                                  popUpTo(it) { saveState = true }
+                              }
+                              launchSingleTop = true
+                              restoreState = true
+                          }
+                },
+                icon = {
+                    Icon(
+                        imageVector = item.icon,
+                        contentDescription = null
+                    )
+                }
+            )
+        }
+    }
+
+}
+
+@Composable
+fun NavigationGraph(navController: NavHostController) {
+    NavHost(navController = navController, startDestination = Routes.Home.route) {
+        composable(Routes.Home.route) {
+            HomeScreen()
+        }
+        composable(Routes.TaskList.route) {
+            TaskListScreen()
+        }
+        composable(Routes.Statistic.route) {
+            StatisticScreen()
+        }
+        composable(Routes.Setting.route) {
+            SettingScreen()
+        }
+    }
+}
+
+enum class Routes(val icon: ImageVector, val route: String) {
+    Home(Icons.Rounded.Home,"home"),
+    TaskList(Icons.Rounded.List,"task_list"),
+    Statistic(Icons.Rounded.Info, "statistic"),
+    Setting(Icons.Rounded.Settings, "setting")
+}
 
 @Preview(showBackground = true)
 @Composable
