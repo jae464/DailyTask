2072c634843624eda6d88f7b7cd195e1cd9506c4
이민재 <>
2023-09-12 01:16:10 +0900

    [FEAT] AddTaskScreen 앱바 UI 구현

--- presentation/src/main/java/com/jae464/presentation/tasks/AddTaskScreen.kt
+++ presentation/src/main/java/com/jae464/presentation/tasks/AddTaskScreen.kt
@@ -1,25 +1,77 @@
 package com.jae464.presentation.tasks
 
+import android.graphics.drawable.Icon
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.WindowInsetsSides
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.only
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Save
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
 const val addTaskScreenRoute = "add_task"
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AddTaskScreen() {
-    Surface(
+    Scaffold(
         modifier = Modifier
             .windowInsetsPadding(
                 WindowInsets.navigationBars.only(WindowInsetsSides.Start + WindowInsetsSides.End)
-            )
-    ) {
-        Text(text = "AddTaskScreen")
+            ),
+        topBar = {
+            AddTaskTopAppBar()
+        }
+    ) { padding ->
+        Column(
+            modifier = Modifier
+                .padding(padding)
+                .fillMaxWidth()
+        ) {
+
+        }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AddTaskTopAppBar() {
+    CenterAlignedTopAppBar(
+        title = {
+            Text(text = "일정 추가")
+        },
+        navigationIcon = {
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(
+                    imageVector = Icons.Default.ArrowBack,
+                    contentDescription = "back",
+                )
+            }
+        },
+        actions = {
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(imageVector = Icons.Default.Save,
+                    contentDescription = "save_task")
+            }
+        },
+        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+            containerColor = Color.Transparent,
+        ),
+    )
 }
