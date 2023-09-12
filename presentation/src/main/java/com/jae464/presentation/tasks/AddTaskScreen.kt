d16d1b5ed53ecde54c783ae0ea178a715f2b4d8d
이민재 <>
2023-09-13 02:02:32 +0900

    [FEAT] 일정 추가 화면 제목 TextField 구현

--- presentation/src/main/java/com/jae464/presentation/tasks/AddTaskScreen.kt
+++ presentation/src/main/java/com/jae464/presentation/tasks/AddTaskScreen.kt
@@ -1,35 +1,60 @@
 package com.jae464.presentation.tasks
 
 import android.graphics.drawable.Icon
+import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.WindowInsetsSides
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Save
+import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusManager
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.input.key.key
+import androidx.compose.ui.input.key.onKeyEvent
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
 
 const val addTaskScreenRoute = "add_task"
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AddTaskScreen() {
+fun AddTaskScreen(modifier: Modifier = Modifier) {
     Scaffold(
         modifier = Modifier
             .windowInsetsPadding(
@@ -39,12 +64,12 @@ fun AddTaskScreen() {
             AddTaskTopAppBar()
         }
     ) { padding ->
-        Column(
-            modifier = Modifier
+        Box(
+            modifier = modifier
                 .padding(padding)
-                .fillMaxWidth()
+                .fillMaxSize()
         ) {
-
+            AddTaskBody(modifier = modifier)
         }
     }
 }
@@ -66,8 +91,10 @@ fun AddTaskTopAppBar() {
         },
         actions = {
             IconButton(onClick = { /*TODO*/ }) {
-                Icon(imageVector = Icons.Default.Save,
-                    contentDescription = "save_task")
+                Icon(
+                    imageVector = Icons.Default.Save,
+                    contentDescription = "save_task"
+                )
             }
         },
         colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
@@ -75,3 +102,60 @@ fun AddTaskTopAppBar() {
         ),
     )
 }
+
+@Composable
+fun AddTaskBody(
+    modifier: Modifier = Modifier
+) {
+    var text by remember { mutableStateOf("") }
+
+    Column(modifier = modifier.padding(8.dp)) {
+        TitleTextField(modifier = modifier)
+        ProgressTimeInput()
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TitleTextField(
+    modifier: Modifier = Modifier
+) {
+
+    var text by remember { mutableStateOf("") }
+    val focusManager = LocalFocusManager.current
+
+    OutlinedTextField(
+        modifier = modifier
+            .fillMaxWidth(),
+        value = text,
+        onValueChange = { newText ->
+            text = newText
+        },
+        placeholder = {
+            Text(
+                text = "제목",
+                style = MaterialTheme.typography.titleLarge,
+                color = MaterialTheme.colorScheme.secondary
+            )
+        },
+        singleLine = true,
+        textStyle = MaterialTheme.typography.titleLarge,
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Text,
+            imeAction = ImeAction.Done
+        ),
+        keyboardActions = KeyboardActions {
+            focusManager.clearFocus()
+        }
+
+    )
+}
+
+@Composable
+fun ProgressTimeInput() {
+
+    Row {
+
+    }
+}
+
