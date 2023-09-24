44b67769c3b9cc4caf6f6527a5f489df4aadacf7
이민재 <>
2023-09-25 01:06:18 +0900

    [FEAT] data 모듈, presentation 모듈 -> domain 모듈 dependency 적용

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -35,6 +35,7 @@ android {
 }
 
 dependencies {
+    implementation(project(":domain"))
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.room.runtime)
--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -48,6 +48,7 @@ android {
 }
 
 dependencies {
+    implementation(project(":domain"))
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.compose)
     implementation(libs.androidx.activity.compose)
