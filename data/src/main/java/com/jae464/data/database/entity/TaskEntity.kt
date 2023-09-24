3bc75b53ccfbba5a0b26093640b39579ee9d8eda
이민재 <jae464@naver.com>
2023-09-25 02:16:25 +0900

    [FEAT] TaskEntity, CategoryEntity 구현중

--- data/src/main/java/com/jae464/data/database/entity/TaskEntity.kt
+++ data/src/main/java/com/jae464/data/database/entity/TaskEntity.kt
@@ -1,5 +1,18 @@
 package com.jae464.data.database.entity
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.jae464.domain.model.HourMinute
+import java.time.LocalDateTime
+
+@Entity
 data class TaskEntity(
-    val name: String
+    @PrimaryKey
+    @ColumnInfo(name = "id")
+    val id: String,
+    @ColumnInfo(name = "title") val title: String,
+    @ColumnInfo(name = "progress_time") val progressTime: HourMinute,
+    @ColumnInfo(name = "category_id") val categoryId: Long,
+    @ColumnInfo(name = "created_at") val createdAt: LocalDateTime
 )
