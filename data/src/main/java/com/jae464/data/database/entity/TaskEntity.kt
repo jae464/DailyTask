c79bc745a0ca30d79c2c84434fcf30d7946001d9
이민재 <>
2023-09-25 22:48:10 +0900

    [FEAT] CategoryEntity, TaskEntity 일대다 관계 형성

--- data/src/main/java/com/jae464/data/database/entity/TaskEntity.kt
+++ data/src/main/java/com/jae464/data/database/entity/TaskEntity.kt
@@ -2,17 +2,32 @@ package com.jae464.data.database.entity
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
+import com.jae464.domain.model.DayOfWeek
 import com.jae464.domain.model.HourMinute
+import com.jae464.domain.model.TaskType
 import java.time.LocalDateTime
 
-@Entity
+@Entity(
+    tableName = "tasks",
+    foreignKeys = [
+        ForeignKey(
+            entity = CategoryEntity::class,
+            parentColumns = ["id"],
+            childColumns = ["category_id"]
+        )
+    ]
+)
 data class TaskEntity(
     @PrimaryKey
     @ColumnInfo(name = "id")
     val id: String,
     @ColumnInfo(name = "title") val title: String,
     @ColumnInfo(name = "progress_time") val progressTime: HourMinute,
+    @ColumnInfo(name = "task_type") val taskType: TaskType,
+    @ColumnInfo(name = "day_of_week") val dayOfWeek: DayOfWeek,
     @ColumnInfo(name = "category_id") val categoryId: Long,
+    @ColumnInfo(name = "alarm_time") val alarmTime: LocalDateTime,
     @ColumnInfo(name = "created_at") val createdAt: LocalDateTime
 )
