1eb50fd8e985f23f0cf0331f8f829e50caed68d4
이민재 <jae464@naver.com>
2023-09-25 23:07:48 +0900

    [FIX] TaskEntity에서 dayOfWeeks 필드 리스트 형태로 변경

--- data/src/main/java/com/jae464/data/database/entity/TaskEntity.kt
+++ data/src/main/java/com/jae464/data/database/entity/TaskEntity.kt
@@ -26,7 +26,7 @@ data class TaskEntity(
     @ColumnInfo(name = "title") val title: String,
     @ColumnInfo(name = "progress_time") val progressTime: HourMinute,
     @ColumnInfo(name = "task_type") val taskType: TaskType,
-    @ColumnInfo(name = "day_of_week") val dayOfWeek: DayOfWeek,
+    @ColumnInfo(name = "day_of_week") val dayOfWeeks: List<DayOfWeek>,
     @ColumnInfo(name = "category_id") val categoryId: Long,
     @ColumnInfo(name = "alarm_time") val alarmTime: LocalDateTime,
     @ColumnInfo(name = "created_at") val createdAt: LocalDateTime
