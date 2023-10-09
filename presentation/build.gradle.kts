c5563ebafbd806cf688b58d52d2fe3d000962f0f
이민재 <jae464@naver.com>
2023-10-09 16:36:43 +0900

    [FEAT] 일정 목록 화면에서 일정 왼쪽으로 스와이프시 삭제 버튼 나타나도록 구현. 삭제 기능 구현.

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -58,6 +58,7 @@ dependencies {
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.material.iconsExtended)
 
