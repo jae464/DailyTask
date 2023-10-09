2285a1d6b6ad36e5f11c46efcb8aa43f0842c503
이민재 <>
2023-10-09 16:36:43 +0900

    [FEAT] 일정 목록 화면에서 일정 왼쪽으로 스와이프시 삭제 버튼 나타나도록 구현. 삭제 기능 구현.

--- presentation/src/main/java/com/jae464/presentation/tasks/TaskListScreen.kt
+++ presentation/src/main/java/com/jae464/presentation/tasks/TaskListScreen.kt
@@ -1,7 +1,14 @@
 package com.jae464.presentation.tasks
 
+import android.util.Log
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -13,18 +20,29 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.DismissDirection
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.FractionalThreshold
+import androidx.compose.material.SwipeToDismiss
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.AccessTimeFilled
 import androidx.compose.material.icons.rounded.AddCircle
+import androidx.compose.material.icons.rounded.Delete
 import androidx.compose.material.icons.rounded.Timer
+import androidx.compose.material.rememberDismissState
+import androidx.compose.material.rememberSwipeableState
+import androidx.compose.material.swipeable
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -36,15 +54,26 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.jae464.presentation.model.TaskUIModel
 import com.jae464.presentation.sampledata.taskUiModels
+import kotlin.math.roundToInt
 
 @Composable
 fun TaskListScreen(
@@ -55,6 +84,7 @@ fun TaskListScreen(
 ) {
 
     val taskState by viewModel.taskState.collectAsStateWithLifecycle()
+    var showDeleteDialog by remember { mutableStateOf(null) }
 
     Surface(
         modifier = Modifier
@@ -81,8 +111,8 @@ fun TaskListScreen(
                             key = { it.id }) { taskUiModel ->
                             TaskItem(
                                 taskUIModel = taskUiModel,
-                                modifier = Modifier.padding(vertical = 4.dp),
-                                onClickTask = onClickTask
+                                onClickTask = onClickTask,
+                                onClickDelete = viewModel::deleteTask
                             )
                         }
                     }
@@ -97,74 +127,156 @@ fun TaskListScreen(
                         )
                     }
                 }
+
+                else -> {}
+
             }
         }
+
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
-fun TaskItem(taskUIModel: TaskUIModel, modifier: Modifier, onClickTask: (String) -> Unit) {
-    Card(
-        shape = RoundedCornerShape(8.dp),
-        elevation = CardDefaults.cardElevation(
-            defaultElevation = 4.dp
-        ),
-        modifier = modifier.fillMaxWidth(),
-        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surface),
-        onClick = { onClickTask(taskUIModel.id) }
-    ) {
-        Column(modifier = Modifier.padding(16.dp)) {
-            Text(
-                text = taskUIModel.header,
-                color = MaterialTheme.colorScheme.tertiary,
-                style = MaterialTheme.typography.bodySmall
+fun SwipeDismissItem(
+    modifier: Modifier = Modifier,
+    directions: Set<DismissDirection> = setOf(DismissDirection.EndToStart),
+    enter: EnterTransition = expandVertically(),
+    exit: ExitTransition = shrinkVertically(),
+    background: @Composable (offset: Dp) -> Unit,
+    content: @Composable (isDismissed: Boolean) -> Unit
+) {
+    val dismissState = rememberDismissState()
+    val isDismissed = dismissState.isDismissed(DismissDirection.EndToStart)
+    val offset = with(LocalDensity.current) { dismissState.offset.value.toDp() }
 
+    AnimatedVisibility(
+        modifier = modifier,
+        visible = !isDismissed,
+        enter = enter,
+        exit = exit
+    ) {
+        SwipeToDismiss(
+            modifier = modifier,
+            state = dismissState,
+            directions = directions,
+            background = { background(offset) },
+            dismissContent = { content(isDismissed) }
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterialApi::class)
+@Composable
+fun TaskItem(
+    taskUIModel: TaskUIModel,
+    modifier: Modifier = Modifier,
+    onClickTask: (String) -> Unit,
+    onClickDelete: (String) -> Unit,
+) {
+    val swipeableState = rememberSwipeableState(initialValue = 0)
+    val scope = rememberCoroutineScope()
+
+    Box(
+        modifier = modifier
+            .clip(RoundedCornerShape(8.dp))
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .swipeable(
+                anchors = mapOf(
+                    0f to 0,
+                    (-200).dp.value to 1
+                ),
+                thresholds = { _, _ ->
+                    FractionalThreshold(0.3f)
+                },
+                state = swipeableState,
+                orientation = Orientation.Horizontal
             )
-            Spacer(modifier = Modifier.height(8.dp))
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.spacedBy(8.dp),
-            ) {
-                taskUIModel.dayOfWeek?.forEach { it ->
-                    RoundedBackgroundText(text = it.day)
+            .background(if (swipeableState.offset.value < 0) Color.Red else Color.White)
+
+    ) {
+
+        IconButton(
+            onClick = {
+                      Log.d("TaskListScreen", "onClick delete button")
+                // TODO show alert dialog to confirm delete
+                onClickDelete(taskUIModel.id)
+            },
+            modifier =
+            Modifier
+                .align(Alignment.CenterEnd)
+                .padding(end = 20.dp)
+                .wrapContentWidth()
+        ) {
+            Image(
+                imageVector = Icons.Rounded.Delete,
+                contentDescription = "delete_task"
+            )
+        }
+
+        Card(
+            modifier = modifier
+                .offset {
+                    IntOffset(swipeableState.offset.value.roundToInt(), 0)
                 }
-            }
-            Spacer(modifier = Modifier.height(8.dp))
-            Text(
-                text = taskUIModel.title,
-                style = MaterialTheme.typography.titleLarge,
-                fontWeight = FontWeight.Bold
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.spacedBy(8.dp),
-            ) {
-                Icon(
-                    imageVector = Icons.Rounded.AccessTimeFilled,
-                    contentDescription = "progress_time",
-                    tint = Color.Black
-                )
+                .fillMaxWidth(),
+            shape = RoundedCornerShape(8.dp),
+            colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.primaryContainer),
+            onClick = { onClickTask(taskUIModel.id) }
+        ) {
+            Column(modifier = Modifier.padding(16.dp)) {
                 Text(
-                    text = taskUIModel.progressTime,
-                    style = MaterialTheme.typography.bodyMedium
+                    text = taskUIModel.header,
+                    color = MaterialTheme.colorScheme.tertiary,
+                    style = MaterialTheme.typography.bodySmall
+
                 )
-            }
-            Spacer(modifier = Modifier.height(8.dp))
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.spacedBy(8.dp),
-            ) {
-                Icon(
-                    imageVector = Icons.Rounded.Timer,
-                    contentDescription = "progress_time",
-                    tint = Color.Black
-                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(8.dp),
+                ) {
+                    taskUIModel.dayOfWeek?.forEach { it ->
+                        RoundedBackgroundText(text = it.day)
+                    }
+                }
+                Spacer(modifier = Modifier.height(8.dp))
                 Text(
-                    text = taskUIModel.alarmTime,
-                    style = MaterialTheme.typography.bodyMedium
+                    text = taskUIModel.title,
+                    style = MaterialTheme.typography.titleLarge,
+                    fontWeight = FontWeight.Bold
                 )
+                Spacer(modifier = Modifier.height(8.dp))
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(8.dp),
+                ) {
+                    Icon(
+                        imageVector = Icons.Rounded.AccessTimeFilled,
+                        contentDescription = "progress_time",
+                        tint = Color.Black
+                    )
+                    Text(
+                        text = taskUIModel.progressTime,
+                        style = MaterialTheme.typography.bodyMedium
+                    )
+                }
+                Spacer(modifier = Modifier.height(8.dp))
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(8.dp),
+                ) {
+                    Icon(
+                        imageVector = Icons.Rounded.Timer,
+                        contentDescription = "progress_time",
+                        tint = Color.Black
+                    )
+                    Text(
+                        text = taskUIModel.alarmTime,
+                        style = MaterialTheme.typography.bodyMedium
+                    )
+                }
             }
         }
     }
