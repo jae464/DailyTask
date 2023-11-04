733b8cc607261a822f1398ffe2f094d048a46f4a
이민재 <jae464@naver.com>
2023-11-04 21:34:36 +0900

    [FEAT] HomeScreen에서 ProgressTask UI 일부 구현

--- presentation/src/main/java/com/jae464/presentation/home/HomeScreen.kt
+++ presentation/src/main/java/com/jae464/presentation/home/HomeScreen.kt
@@ -1,36 +1,49 @@
 package com.jae464.presentation.home
 
 import android.util.Log
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.WindowInsetsSides
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.key
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.jae464.domain.model.HourMinute
 
 @Composable
 fun HomeScreen(
@@ -47,25 +60,8 @@ fun HomeScreen(
             )
             .fillMaxSize(),
     ) {
-//        Box(modifier = modifier.fillMaxSize()) {
-//            when (progressTaskState) {
-//                is ProgressTaskState.Success -> {
-//                    LazyColumn(verticalArrangement = Arrangement.spacedBy(8.dp)) {
-//                        items(
-//                            (progressTaskState as ProgressTaskState.Success).progressTasks,
-//                            key = null
-//                        ) {progressTaskUiModel ->
-//                            ProgressTaskItem(progressTaskUiModel = progressTaskUiModel)
-//                        }
-//
-//                    }
-//                }
-//
-//                else -> {}
-//            }
-//        }
-        ProgressTaskList(progressTaskState = progressTaskState)
 
+        ProgressTaskList(progressTaskState = progressTaskState)
     }
 }
 
@@ -74,14 +70,19 @@ fun ProgressTaskList(
     modifier: Modifier = Modifier,
     progressTaskState: ProgressTaskState
 ) {
-    Box(modifier = modifier.fillMaxSize()) {
+    Log.d("ProgressTaskList", progressTaskState.toString())
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(horizontal = 16.dp)
+    ) {
         when (progressTaskState) {
             is ProgressTaskState.Success -> {
                 LazyColumn(verticalArrangement = Arrangement.spacedBy(8.dp)) {
                     items(
                         progressTaskState.progressTasks,
                         key = null
-                    ) {progressTaskUiModel ->
+                    ) { progressTaskUiModel ->
                         ProgressTaskItem(progressTaskUiModel = progressTaskUiModel)
                     }
 
@@ -104,15 +105,59 @@ fun ProgressTaskItem(
             .wrapContentHeight()
     ) {
         Card(
-            modifier = modifier.fillMaxWidth(),
+            modifier = modifier
+                .fillMaxWidth(),
             shape = RoundedCornerShape(8.dp),
-            colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.primaryContainer),
+            colors = CardDefaults.cardColors(containerColor = Color.White),
         ) {
-            Column(modifier = Modifier.padding(16.dp)) {
-                Text(text = progressTaskUiModel.categoryName)
-                Spacer(modifier = Modifier.height(8.dp))
-                Text(text = progressTaskUiModel.title)
+            Row(
+                modifier = modifier.padding(8.dp),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                RoundedTimer(
+                    hourMinute = progressTaskUiModel.totalTime
+                )
+                Column(modifier = Modifier
+                    .padding(16.dp)
+                    .weight(1f)) {
+                    Text(
+                        text = progressTaskUiModel.categoryName,
+                        color = MaterialTheme.colorScheme.tertiary,
+                        style = MaterialTheme.typography.bodySmall,
+                    )
+                    Spacer(modifier = Modifier.height(4.dp))
+                    Text(
+                        text = progressTaskUiModel.title,
+                        style = MaterialTheme.typography.titleMedium,
+                        fontWeight = FontWeight.Bold,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                }
+                TextButton(onClick = { /*TODO*/ }) {
+                    Text(text = "시작")
+                }
             }
         }
+    }
+}
+
+@Composable
+fun RoundedTimer(
+    modifier: Modifier = Modifier,
+    hourMinute: HourMinute
+) {
+    Box(
+        modifier
+            .size(64.dp)
+            .background(Color.Black, CircleShape)
+            .padding(4.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "${hourMinute.hour}:${hourMinute.minute}",
+            style = MaterialTheme.typography.bodyMedium,
+            color = Color.White
+        )
     }
 }
