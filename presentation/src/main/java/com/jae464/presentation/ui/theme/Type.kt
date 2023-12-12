5acddc9694c9e71813f9192b3f44fa954fc43865
이민재 <>
2023-12-13 01:57:32 +0900

    [DESIGN] notosans 폰트 적용 및 일부 패딩 수정

--- presentation/src/main/java/com/jae464/presentation/ui/theme/Type.kt
+++ presentation/src/main/java/com/jae464/presentation/ui/theme/Type.kt
@@ -2,33 +2,43 @@ package com.jae464.presentation.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.jae464.presentation.R
+
+val notosans = FontFamily(
+    Font(R.font.notosans_bold, FontWeight.Bold, FontStyle.Normal),
+    Font(R.font.notosans_medium, FontWeight.Medium, FontStyle.Normal),
+    Font(R.font.notosans_regular, FontWeight.Normal, FontStyle.Normal),
+    Font(R.font.notosans_light, FontWeight.Light, FontStyle.Normal),
+    Font(R.font.notosans_semibold, FontWeight.Thin, FontStyle.Normal)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = notosans,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+    ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = notosans,
         fontWeight = FontWeight.Normal,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = FontFamily.Monospace,
         fontWeight = FontWeight.Medium,
         fontSize = 11.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     )
-    */
+
 )
