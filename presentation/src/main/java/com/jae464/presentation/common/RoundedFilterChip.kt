e35ab8b00d6a53715779032666cd5313880966e1
이민재 <jae464@naver.com>
2023-12-15 19:19:04 +0900

    [FEAT] 일정 목록 화면 카테고리 필터링 칩 추가 및 기능 구현

---
+++ presentation/src/main/java/com/jae464/presentation/common/RoundedFilterChip.kt
@@ -0,0 +1,38 @@
+package com.jae464.presentation.common
+
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilterChip
+import androidx.compose.material3.FilterChipDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SelectableChipBorder
+import androidx.compose.material3.SelectableChipColors
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun RoundedFilterChip(
+    text: String,
+    checked: Boolean,
+    onCheckedChanged: (Boolean) -> Unit,
+    colors: SelectableChipColors = FilterChipDefaults.filterChipColors(),
+    border: SelectableChipBorder? = FilterChipDefaults.filterChipBorder(),
+) {
+    FilterChip(
+        modifier = Modifier.wrapContentSize(),
+        selected = checked,
+        onClick = {
+            onCheckedChanged(!checked)
+        },
+        label = {
+            Text(text = text, style = MaterialTheme.typography.labelSmall)
+        },
+        shape = CircleShape,
+        colors = colors,
+        border = border
+    )
+
+}
