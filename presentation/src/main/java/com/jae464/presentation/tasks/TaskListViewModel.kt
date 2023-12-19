8d02c0ed59b2cb4061279c466b11e35f518b970e
이민재 <jae464@naver.com>
2023-12-19 15:31:13 +0900

    [FEAT] 일정목록 화면 툴바에 검색 기능 구현

--- presentation/src/main/java/com/jae464/presentation/tasks/TaskListViewModel.kt
+++ presentation/src/main/java/com/jae464/presentation/tasks/TaskListViewModel.kt
@@ -10,14 +10,18 @@ import com.jae464.presentation.home.ProgressingTaskManager
 import com.jae464.presentation.model.TaskUiModel
 import com.jae464.presentation.model.toTaskUiModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+@OptIn(FlowPreview::class)
 @HiltViewModel
 class TaskListViewModel @Inject constructor(
     private val getAllTasksUseCase: GetAllTasksUseCase,
@@ -41,22 +45,47 @@ class TaskListViewModel @Inject constructor(
             initialValue = emptyList()
         )
 
-    private var _filteredCategories = MutableStateFlow<List<Category>>(emptyList())
+    private val _filteredCategories = MutableStateFlow<List<Category>>(emptyList())
     val filteredCategories: StateFlow<List<Category>>
         get() = _filteredCategories
 
+
+    private val _searchText = MutableStateFlow("")
+    val searchText: StateFlow<String>
+        get() = _searchText
+
+    private val filteredSearchText = MutableStateFlow("")
+
+    init {
+        viewModelScope.launch {
+            searchText.debounce(500).collect {
+                filteredSearchText.value = it
+            }
+        }
+    }
+
     val taskListUiState: StateFlow<TaskListUiState> =
-        combine(categories, tasks, filteredCategories) { categories, tasks, filteredCategories ->
+        combine(
+            categories,
+            tasks,
+            filteredCategories,
+            filteredSearchText
+        ) { categories, tasks, filteredCategories, filteredSearchText ->
             if (categories.isNotEmpty()) {
                 if (tasks.isEmpty()) {
                     TaskListUiState.Empty
                 } else {
-                    TaskListUiState.Success(tasks.filter {
-                        filteredCategories.isEmpty() || filteredCategories.map { fc -> fc.id }
-                            .contains(it.categoryId)
-                    }.map { task ->
-                        task.toTaskUiModel(categories.first { it.id == task.categoryId }.name)
-                    })
+                    TaskListUiState.Success(tasks
+                        .filter {
+                            filteredCategories.isEmpty() || filteredCategories.map { fc -> fc.id }
+                                .contains(it.categoryId)
+                        }
+                        .filter {
+                            filteredSearchText.isEmpty() || it.title.contains(filteredSearchText)
+                        }
+                        .map { task ->
+                            task.toTaskUiModel(categories.first { it.id == task.categoryId }.name)
+                        })
                 }
             } else {
                 TaskListUiState.Loading
@@ -79,6 +108,10 @@ class TaskListViewModel @Inject constructor(
 
     fun filterCategories(filteredCategories: List<Category>) {
         _filteredCategories.value = filteredCategories
+    }
+
+    fun setSearchText(text: String) {
+        _searchText.value = text
     }
 
 }
