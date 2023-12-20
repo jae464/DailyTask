e91c5a24b498067a4f6ac0ce106282dd9c584cb5
이민재 <jae464@naver.com>
2023-12-21 01:06:41 +0900

    [FEAT] CustomCalendar 컴포저블 CalendarState 및 기본 틀 구현

---
+++ presentation/src/main/java/com/jae464/presentation/common/calendar/CalendarState.kt
@@ -0,0 +1,107 @@
+package com.jae464.presentation.common.calendar
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.Saver
+import androidx.compose.runtime.saveable.listSaver
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import java.time.LocalDate
+
+@Composable
+fun rememberCalendarState(
+    startDate: LocalDate? = null,
+    endDate: LocalDate? = null,
+    selectState: CalendarSelectState = CalendarSelectState.DAY,
+    selectedYear: Int = LocalDate.now().year,
+    selectedMonth: Int = LocalDate.now().month.value,
+): CalendarState {
+    return rememberSaveable(saver = CalendarState.Saver) {
+        CalendarState(
+            startDate = startDate,
+            endDate = endDate,
+            selectState = selectState,
+            selectedYear = selectedYear,
+            selectedMonth = selectedMonth
+        )
+    }
+}
+
+@Stable
+class CalendarState constructor(
+    startDate: LocalDate? = null,
+    endDate: LocalDate? = null,
+    selectState: CalendarSelectState = CalendarSelectState.DAY,
+    selectedYear: Int = LocalDate.now().year,
+    selectedMonth: Int = LocalDate.now().month.value,
+){
+    private var _startDate by mutableStateOf(startDate)
+    var startDate: LocalDate?
+        get() = _startDate
+        set(value) {
+            _startDate = value
+        }
+
+    private var _endDate by mutableStateOf(endDate)
+    var endDate: LocalDate?
+        get() = _endDate
+        set(value) {
+            _endDate = value
+        }
+
+    private var _selectState by mutableStateOf(selectState)
+    var selectState: CalendarSelectState
+        get() = _selectState
+        set(value) {
+            _selectState = value
+        }
+
+    private var _selectedYear by mutableStateOf(selectedYear)
+    var selectedYear: Int
+        get() = _selectedYear
+        set(value) {
+            _selectedYear = value
+        }
+
+    private var _selectedMonth by mutableStateOf(selectedMonth)
+    var selectedMonth: Int
+        get() = _selectedMonth
+        set(value) {
+            _selectedMonth = value
+        }
+
+
+
+
+
+
+    companion object {
+        internal val Saver: Saver<CalendarState, Any> = listSaver(
+            save = {
+                listOf(
+                    it.startDate,
+                    it.endDate,
+                    it.selectState,
+                    it.selectedYear,
+                    it.selectedMonth
+                )
+            },
+            restore = {
+                CalendarState(
+                    startDate = it[0] as LocalDate?,
+                    endDate = it[1] as LocalDate?,
+                    selectState = it[2] as CalendarSelectState,
+                    selectedYear = it[3] as Int,
+                    selectedMonth = it[4] as Int
+                )
+            }
+        )
+    }
+}
+
+enum class CalendarSelectState {
+    YEAR, MONTH, DAY
+}
+
