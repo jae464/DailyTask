123c0ff1038473445cebdae411c99154631f79a4
10152954 <minjae12.lee@kt.com>
2023-12-21 14:07:03 +0900

    [FEAT] Custom Calendar UI 구현. 구간선택 기능 구현

--- presentation/src/main/java/com/jae464/presentation/setting/SettingScreen.kt
+++ presentation/src/main/java/com/jae464/presentation/setting/SettingScreen.kt
@@ -6,12 +6,14 @@ import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.only
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.jae464.presentation.common.calendar.CustomCalendar
 
 @Composable
@@ -24,7 +26,9 @@ fun SettingScreen() {
             .fillMaxSize(),
         color = MaterialTheme.colorScheme.surface
     ) {
-        Box {
+        Box(
+            modifier = Modifier.padding(horizontal = 16.dp, vertical = 16.dp)
+        ) {
             CustomCalendar()
         }
     }
