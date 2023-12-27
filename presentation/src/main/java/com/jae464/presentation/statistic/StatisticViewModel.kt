d7c99dd712de6bf218185d35baaa60ca02a4f94a
이민재 <jae464@naver.com>
2023-12-27 21:53:10 +0900

    [FEAT] 통계화면 리스트 UI 구현, 통계 화면에서 사용할 TotalProgressTaskUiModel 생성 및 적용

--- presentation/src/main/java/com/jae464/presentation/statistic/StatisticViewModel.kt
+++ presentation/src/main/java/com/jae464/presentation/statistic/StatisticViewModel.kt
@@ -1,9 +1,12 @@
 package com.jae464.presentation.statistic
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.jae464.domain.model.ProgressTask
 import com.jae464.domain.usecase.progresstask.GetProgressTaskByDates
+import com.jae464.presentation.statistic.model.TotalProgressTaskUiModel
+import com.jae464.presentation.statistic.model.toTotalProgressTaskUiModels
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.collectLatest
@@ -16,14 +19,27 @@ class StatisticViewModel @Inject constructor(
     private val getProgressTaskByDates: GetProgressTaskByDates
 ) : ViewModel() {
 
-    val progressTasks = MutableStateFlow<List<ProgressTask>>(emptyList())
+    val totalProgressTasksUiState = MutableStateFlow<TotalProgressTasksUiState>(TotalProgressTasksUiState.Empty)
 
     fun getProgressTasks(startDate: LocalDate, endDate: LocalDate) {
         viewModelScope.launch {
+            totalProgressTasksUiState.value = TotalProgressTasksUiState.Loading
+            // TODO 호출할때마다 Collector가 계속해서 증가할 것으로 보임. 수정 필요
             getProgressTaskByDates(startDate, endDate).collectLatest {
-                progressTasks.value = it
+                Log.d("StatisticViewModel", "collector ${this.coroutineContext.hashCode()}")
+                if (it.isEmpty()) {
+                    totalProgressTasksUiState.value = TotalProgressTasksUiState.Empty
+                }
+                else {
+                    totalProgressTasksUiState.value = TotalProgressTasksUiState.Success(it.toTotalProgressTaskUiModels())
+                }
             }
         }
     }
+}
 
+sealed interface TotalProgressTasksUiState {
+    object Empty: TotalProgressTasksUiState
+    object Loading: TotalProgressTasksUiState
+    data class Success(val totalProgressTasks: List<TotalProgressTaskUiModel>): TotalProgressTasksUiState
 }
