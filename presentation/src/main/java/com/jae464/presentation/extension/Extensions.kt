5e39a9e0442e22fbf404ae8f3601f646df895f7c
10152954 <minjae12.lee@kt.com>
2024-01-18 13:36:10 +0900

    [FIX] 키보드가 올라와 있는 상태에서 일정 아이템 클릭시 일정 화면으로 넘어가지 않고 키보드만 숨기도록 수정

--- presentation/src/main/java/com/jae464/presentation/extension/Extensions.kt
+++ presentation/src/main/java/com/jae464/presentation/extension/Extensions.kt
@@ -1,19 +1,19 @@
 package com.jae464.presentation.extension
 
 import android.util.Log
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.gestures.detectVerticalDragGestures
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusManager
 import androidx.compose.ui.input.pointer.pointerInput
 
 fun Modifier.addFocusCleaner(focusManager: FocusManager, doOnClear: () -> Unit = {} ): Modifier {
     return this.pointerInput(Unit) {
-        detectTapGestures(
-            onTap = {
-                Log.d("Extensions", "onTap")
-                doOnClear()
-                focusManager.clearFocus()
-            }
-        )
+        detectTapGestures {
+            Log.d("Extensions", "onPress")
+            doOnClear()
+            focusManager.clearFocus()
+        }
     }
 }
