0bb6b0cc8d7fe2bc3962f2ab62ac251168d2b4d2
이민재 <>
2024-03-11 00:48:28 +0900

    [FEAT] 설정창 UI 일부 구현

--- presentation/src/main/java/com/jae464/presentation/setting/SettingScreen.kt
+++ presentation/src/main/java/com/jae464/presentation/setting/SettingScreen.kt
@@ -1,33 +1,34 @@
 package com.jae464.presentation.setting
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.windowInsetsPadding
-import androidx.compose.material3.Button
-import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import com.jae464.presentation.common.calendar.CalendarMode
-import com.jae464.presentation.common.calendar.CustomCalendar
-import com.jae464.presentation.common.calendar.rememberCalendarState
 
 @Composable
 fun SettingScreen(
     onClickTestScreen: () -> Unit
 ) {
-    val calendarState = rememberCalendarState(
-        calendarMode = CalendarMode.MULTISELECT
-    )
     Surface(
         modifier = Modifier
             .windowInsetsPadding(
@@ -37,18 +38,58 @@ fun SettingScreen(
         color = MaterialTheme.colorScheme.surface
     ) {
         Box(
-            modifier = Modifier.padding(horizontal = 16.dp, vertical = 16.dp)
+            modifier = Modifier.padding(vertical = 16.dp)
         ) {
             Column {
-                CustomCalendar(
-                    calendarState = calendarState
-                )
-                CircularProgressIndicator()
-                Button(onClick = onClickTestScreen) {
-                    Text(text = "테스트 화면으로 이동하기")
-                }
-            }
+//                Button(onClick = onClickTestScreen) {
+//                    Text(text = "테스트 화면으로 이동하기")
+//                }
+                SettingList()
 
+            }
         }
     }
 }
+
+@Composable
+fun SettingList() {
+    Column(
+        modifier = Modifier
+            .background(Color.White)
+            .wrapContentHeight()
+            .fillMaxWidth()
+    ) {
+        SettingItem(title = "화면테마")
+        SettingItem(title = "카테고리")
+    }
+}
+
+@Composable
+fun SettingItem(
+    title: String,
+    onClickItem: () -> Unit = {}
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp),
+    ) {
+        Spacer(modifier = Modifier.height(16.dp))
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(text = title,
+                style = MaterialTheme.typography.bodyLarge
+            )
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Divider(
+            color = MaterialTheme.colorScheme.surface,
+            thickness = 1.dp
+        )
+    }
+}
+
+
